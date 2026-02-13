a0bda424d34d817239e125336f6d27687f475eb5
luiz_ <luiz_carvalho20@hotmail.com>
2026-02-13 10:38:34 -0300

    ci: adiciona pipeline com testes de backend e e2e

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -2,6 +2,6 @@ import { app } from "./app";
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3001;
 
-app.listen(PORT, () => {
-  console.log(`Backend rodando em http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend rodando em http://0.0.0.0:${PORT}`);
 });
